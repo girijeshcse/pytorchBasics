19561944054238a56dff002e85d3ce85d4f0860c
Girijesh Prasad <girijeshcse@gmail.com>
2021-09-12 13:33:30 +0530

    nn with pytorch

--- train.py
+++ train.py
@@ -12,7 +12,7 @@ import torch
 
 def next_batch(inputs, targets, batchSize):
     # loop over the dataset
-    for i in range(0, inputs.shaoe[0], batchSize):
+    for i in range(0, inputs.shape[0], batchSize):
         # yield a tuple of the current batched data and labels
         yield inputs[i:i + batchSize], targets[i:i + batchSize]
 
@@ -34,3 +34,75 @@ print("[INFO] preparing data...")
 # create training and testing splits, and convert them to PyTorch tensors
 trainX, testX, trainY, testY = train_test_split(X, y, test_size=0.15, random_state=95)
 trainX = torch.from_numpy(trainX).float()
+trainY = torch.from_numpy(trainY).float()
+testX = torch.from_numpy(testX).float()
+testY = torch.from_numpy(testY).float()
+
+# initialize our model and display its architecture
+mlp = mlp.get_training_model().to(DEVICE)
+print(mlp)
+
+# initialize optimizer and loss function
+opt = SGD(mlp.parameters(), lr=LR)
+lossFunc = nn.CrossEntropyLoss()
+
+# create a template to summarize current training progress
+trainTemplate = "epoch: {} test loss: {:.3f} test accuracy: {:.3f}"
+
+# loop through the each epochs
+for epoch in range(0, EPOCHS):
+    # initialize tracker variables and set our model to trainable
+    print("[INFO] epoch: {}...".format(epoch + 1))
+    trainLoss = 0
+    trainAcc = 0
+    samples = 0
+    mlp.train()
+
+    # loop over the current batch of data
+    for (batchX, batchY) in next_batch(trainX, trainY, BATCH_SIZE):
+        # flash data to the current device, run it through our model, and calculate loss
+        (batchX, batchY) = (batchX.to(DEVICE)), (batchY.to(DEVICE))
+        #print(type(batchX), type(batchY))
+        predictions = mlp(batchX)
+        loss = lossFunc(predictions, batchY.long())
+
+        # zero the gradients accumulated from the previous steps, perform backpropagation, and update model parameters
+        opt.zero_grad()
+        loss.backward()
+        opt.step()
+
+        # update training loss, accuracy, and the number of samples visited
+        trainLoss += loss.item() * batchY.size(0)
+        trainAcc += (predictions.max(1)[1] == batchY).sum().item()
+        samples += batchY.size(0)
+    # display model progress on the current training batch
+    trainTemplate = "epoch: {} train loss: {:.3f} train accuracy: {:.3f}"
+    print(trainTemplate.format(epoch + 1, (trainLoss / samples), (trainAcc / samples)))
+
+    # initialize tracker variables for testing, then set our model to evaluation mode
+    testLoss = 0
+    testAcc = 0
+    samples = 0
+    mlp.eval()
+
+    # initialize a no-gradient context
+    with torch.no_grad():
+        # loop over the current batch of test data
+        for (batchX, batchY) in next_batch(testX, testY, BATCH_SIZE):
+            # flash the data to the current device
+            (batchX, batchY) = (batchX.to(DEVICE)), (batchY.to(DEVICE))
+            #print(type(batchX), type(batchY))
+            # run data through our model and calculate loss
+            predictions = mlp(batchX)
+            loss = lossFunc(predictions, batchY.long())
+
+            # update test loss, accuracy, and the number of samples visited
+            testLoss += loss.item() * batchY.size(0)
+            testAcc += (predictions.max(1)[1] == batchY).sum().item()
+            samples += batchY.size(0)
+
+        # display model progress on the current test batch
+        testTemplate = "epoch: {} test loss: {:.3f} test accuracy: {:.3f}"
+        print(testTemplate.format(epoch + 1, (testLoss / samples),(testAcc / samples)))
+        print("")
+
